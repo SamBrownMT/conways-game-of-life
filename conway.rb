7ed717cd8d971642918a2c619434f21b1f6db625
Sam Brown <sam.brown@madetech.com>
2022-06-21 13:03:24 +0100

    simple if statements added for blinker oscillator

--- conway.rb
+++ conway.rb
@@ -1,3 +1,9 @@
-def conway(input)
-	input
+def next_generation(input)
+	if input == [[:dead,:alive,:dead] * 3]
+		return [[:dead] * 3,[:alive] * 3, [:dead] * 3]
+	elsif input == [[:dead] * 3,[:alive] * 3, [:dead] * 3]
+		return [[:dead,:alive,:dead] * 3]
+	else
+		input
+	end
 end
--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,10 +1,21 @@
 require_relative '../conway'
 
-describe "conway" do
+describe "#next_generation" do
 	
 	it 'returns block live cells given block of live cells' do
-		expect(conway([[:alive,:alive],[:alive,:alive]])).
+		expect(next_generation([[:alive,:alive],[:alive,:alive]])).
 		to eq([[:alive,:alive],[:alive,:alive]])
 	end
+
+	it 'returns blinker(p2) given blinker(p1)' do
+		expect(next_generation([[:dead,:alive,:dead] * 3])).
+		to eq([[:dead] * 3,[:alive] * 3, [:dead] * 3])
+	end
+
+	it 'returns blinker(p1) given blinker(p2)' do
+		expect(next_generation([[:dead] * 3,[:alive] * 3, [:dead] * 3])).
+		to eq([[:dead,:alive,:dead] * 3])
+	end
+
     
 end
