8ebef19a1cca5b45f8441b716dd16938b9f0a134
Sam Brown <sam.brown@madetech.com>
2022-06-22 13:20:46 +0100

    wrapped next_generation code into loop

--- conway.rb
+++ conway.rb
@@ -1,31 +1,16 @@
 def next_generation(grid)
-	# if input == [[:dead,:alive,:dead] * 3]
-	# 	return [[:dead] * 3,[:alive] * 3, [:dead] * 3]
-	# elsif input == [[:dead] * 3,[:alive] * 3, [:dead] * 3]
-	# 	return [[:dead,:alive,:dead] * 3]
-	# else
-	# 	input
-	# end
 
 	output = Array.new(grid.length) {Array.new(grid[0].length)}
 
-	zero_zero_neighbours = find_neighbours(grid,[0,0])
-	zero_zero_new_status = set_cell_status(grid[0][0],zero_zero_neighbours)
-	output[0][0] = zero_zero_new_status
-
-	zero_one_neighbours = find_neighbours(grid,[0,1])
-	zero_one_new_status = set_cell_status(grid[0][1],zero_one_neighbours)
-	output[0][1] = zero_one_new_status
-
-	one_zero_neighbours = find_neighbours(grid,[1,0])
-	one_zero_new_status = set_cell_status(grid[1][0],one_zero_neighbours)
-	output[1][0] = one_zero_new_status
-
-	one_one_neighbours = find_neighbours(grid,[1,1])
-	one_one_new_status = set_cell_status(grid[1][1],one_one_neighbours)
-	output[1][1] = one_one_new_status
-
-	output
+	output.each_index { |row| 
+		column = 0
+		while column < grid[0].length 
+			neighbours = find_neighbours(grid,[row,column])
+			new_status = set_cell_status(grid[row][column],neighbours)
+			output[row][column] = new_status
+			column += 1
+		end
+	}
 
 end
 
