9174966360edc41b59cccbe87e140327ceb06888
Sam Brown <sam.brown@madetech.com>
2022-06-23 14:03:28 +0100

    wrap methods into class

--- conway.rb
+++ conway.rb
@@ -1,71 +1,72 @@
 
 class ConwayGame
+
 	attr_accessor :grid
+	
 	def initialize(grid)
 		@grid = grid
 	end
 
-end
+	def next_generation(grid)
 
-def next_generation(grid)
+		output = Array.new(grid.length) {Array.new(grid[0].length)}
 
-	output = Array.new(grid.length) {Array.new(grid[0].length)}
+		output.each_index { |row| 
+			column = 0
+			while column < grid[0].length 
+				neighbours = find_neighbours(grid,[row,column])
+				new_status = set_cell_status(grid[row][column],neighbours)
+				output[row][column] = new_status
+				column += 1
+			end
+		}
 
-	output.each_index { |row| 
-		column = 0
-		while column < grid[0].length 
-			neighbours = find_neighbours(grid,[row,column])
-			new_status = set_cell_status(grid[row][column],neighbours)
-			output[row][column] = new_status
-			column += 1
+	end
+
+	def find_neighbours(grid,coords)
+
+		neighbours = []
+
+		unless coords[0] == grid[0].length
+			neighbours.push(grid[coords[0]][coords[1] + 1])
 		end
-	}
 
-end
-
-def find_neighbours(grid,coords)
-
-	neighbours = []
-
-	unless coords[0] == grid[0].length
-		neighbours.push(grid[coords[0]][coords[1] + 1])
-	end
-
-	unless coords[1] == 0
-		neighbours.push(grid[coords[0]][coords[1] - 1])
-	end
-
-	unless coords[0] == grid.length - 1
-		neighbours.push(grid[coords[0] + 1][coords[1]])
-		neighbours.push(grid[coords[0] + 1][coords[1] + 1])
-	end
-
-	unless coords[0] == 0
-		neighbours.push(grid[coords[0] - 1][coords[1]])
-		neighbours.push(grid[coords[0] - 1][coords[1] + 1])
-	end
-
-	unless coords[0] == 0 || coords[1] == 0
-		neighbours.push(grid[coords[0] - 1][coords[1] - 1])
-	end
-
-	unless coords[1] == 0 || coords[0] == grid.length - 1
-		neighbours.push(grid[coords[0] + 1][coords[1] - 1])
-	end
-
-	neighbours.select { |e| e != nil  }
-
-end
-
-def set_cell_status(current_status,neighbours)
-
-	if current_status == :alive
-		if neighbours.count(:alive) < 2 || neighbours.count(:alive) > 3 
-			return :dead
-		else return :alive
+		unless coords[1] == 0
+			neighbours.push(grid[coords[0]][coords[1] - 1])
 		end
-	else
-		return neighbours.count(:alive) == 3 ? :alive : :dead
+
+		unless coords[0] == grid.length - 1
+			neighbours.push(grid[coords[0] + 1][coords[1]])
+			neighbours.push(grid[coords[0] + 1][coords[1] + 1])
+		end
+
+		unless coords[0] == 0
+			neighbours.push(grid[coords[0] - 1][coords[1]])
+			neighbours.push(grid[coords[0] - 1][coords[1] + 1])
+		end
+
+		unless coords[0] == 0 || coords[1] == 0
+			neighbours.push(grid[coords[0] - 1][coords[1] - 1])
+		end
+
+		unless coords[1] == 0 || coords[0] == grid.length - 1
+			neighbours.push(grid[coords[0] + 1][coords[1] - 1])
+		end
+
+		neighbours.select { |e| e != nil  }
+
 	end
 
+	def set_cell_status(current_status,neighbours)
+
+		if current_status == :alive
+			if neighbours.count(:alive) < 2 || neighbours.count(:alive) > 3 
+				return :dead
+			else return :alive
+			end
+		else
+			return neighbours.count(:alive) == 3 ? :alive : :dead
+		end
+
+	end
 end
--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,125 +1,132 @@
 require_relative '../conway'
 
-describe "#next_generation" do
-	
-	it 'returns block live cells given block of live cells' do
-		expect(next_generation([[:alive,:alive],[:alive,:alive]])).
-		to eq([[:alive,:alive],[:alive,:alive]])
-	end
-
-	it 'returns blinker(p2) given blinker(p1)' do
-		expect(next_generation([[:dead,:alive,:dead],
-			[:dead,:alive,:dead],[:dead,:alive,:dead]])).
-		to eq([[:dead,:dead,:dead],[:alive,:alive,:alive],
-		 [:dead,:dead,:dead]])
-	end
-
-	it 'returns blinker(p1) given blinker(p2)' do
-		expect(next_generation([[:dead,:dead,:dead],[:alive,:alive,:alive],
-		 [:dead,:dead,:dead]])).
-		to eq([[:dead,:alive,:dead],
-			[:dead,:alive,:dead],[:dead,:alive,:dead]])
-	end
-
-	it 'returns toad(p2) given toad(p1)' do
-		expect(next_generation([[:dead,:dead,:dead,:dead],
-			[:dead,:alive,:alive,:alive], [:alive,:alive,:alive,:dead],
-			[:dead,:dead,:dead,:dead]])).
-		to eq([[:dead,:dead,:alive,:dead],[:alive,:dead,:dead,:alive],
-			[:alive,:dead,:dead,:alive],[:dead,:alive,:dead,:dead]])
-	end
-
-    
-end
-
-describe "#find_neighbours" do
-	
-	it 'returns b for [[a,b]] and 0,0' do 
-		expect(find_neighbours([['a','b']],[0,0])).to eq ['b']
-	end
-
-	it 'returns a,c for [[a,b,c]] and 0,1' do 
-		expect(find_neighbours([['a','b','c']],[0,1])).
-		to match_array ['a','c']
-	end
-
-	it 'returns b,c,d for [[a,b],[c,d]] and 0,0' do 
-		expect(find_neighbours([['a','b'],['c','d']],[0,0])).
-		to match_array ['b','c','d']
-	end
-
-	it 'returns a,b,d for [[a,b],[c,d]] and 1,0' do 
-		expect(find_neighbours([['a','b'],['c','d']],[1,0])).
-		to match_array ['a','b','d']
-	end
-
-	it 'returns a,b,c for [[a,b],[c,d]] and 1,1' do 
-		expect(find_neighbours([['a','b'],['c','d']],[1,1])).
-		to match_array ['a','b','c']
-	end
-
-	it 'returns a,c,d for [[a,b],[c,d]] and 0,1' do 
-		expect(find_neighbours([['a','b'],['c','d']],[0,1])).
-		to match_array ['a','c','d']
-	end
-
-	it 'returns 1,2,3,4,6,7,8,9 for [[1,2,3],[4,5,6],[7,8,9]] and 1,1' do 
-		expect(find_neighbours([[1,2,3],[4,5,6],[7,8,9]],[1,1])).
-		to match_array [1,2,3,4,6,7,8,9]
-	end
-
-	it 'returns 2,5,6 for [[1,2,3],[4,5,6],[7,8,9]] and 0,2' do 
-		expect(find_neighbours([[1,2,3],[4,5,6],[7,8,9]],[0,2])).
-		to match_array [2,5,6]
-	end
-end
-
-describe '#set_cell_status' do 
-
-	it 'returns :alive for :alive and [:alive,:alive,:alive]' do 
-		expect(set_cell_status(:alive,[:alive,:alive,:alive])).
-		to eq :alive
-	end
-
-	it 'returns :dead for :dead and [:dead,:dead,:alive]' do 
-		expect(set_cell_status(:dead,[:dead,:dead,:alive])).
-		to eq :dead
-	end
-
-	it 'returns :dead for :dead and [:dead,:alive,:dead]' do 
-		expect(set_cell_status(:dead,[:dead,:alive,:dead])).
-		to eq :dead
-	end
-
-	it 'returns :alive for :alive [:alive,:alive,:dead]' do 
-		expect(set_cell_status(:alive,[:alive,:alive,:dead])).
-		to eq :alive
-	end
-
-	it 'returns :dead for :dead [:alive,:alive,:dead]' do 
-		expect(set_cell_status(:dead,[:alive,:alive,:dead])).
-		to eq :dead
-	end
-
-	it 'returns :alive for :dead [:alive,:alive,:alive]' do 
-		expect(set_cell_status(:dead,[:alive,:alive,:alive])).
-		to eq :alive
-	end
-
-	it 'returns :dead for :alive [:dead,:dead,:alive]' do 
-		expect(set_cell_status(:alive,[:dead,:dead,:alive])).
-		to eq :dead
-	end
-
-	it 'returns :dead for :alive [:alive,:alive,:alive,:alive]' do 
-		expect(set_cell_status(:alive,[:alive,:alive,:alive,:alive])).
-		to eq :dead
-	end
-
-end
-
 describe ConwayGame do
+
 	it 'initializes a grid for new instance' do 
 		expect(ConwayGame.new([['foo']]).grid).to be_truthy
+	end
+
+	context "#next_generation" do
+
+		subject = ConwayGame.new([['foo']])
+		
+		it 'returns block live cells given block of live cells' do
+			expect(subject.next_generation([[:alive,:alive],[:alive,:alive]])).
+			to eq([[:alive,:alive],[:alive,:alive]])
+		end
+
+		it 'returns blinker(p2) given blinker(p1)' do
+			expect(subject.next_generation([[:dead,:alive,:dead],
+				[:dead,:alive,:dead],[:dead,:alive,:dead]])).
+			to eq([[:dead,:dead,:dead],[:alive,:alive,:alive],
+			 [:dead,:dead,:dead]])
+		end
+
+		it 'returns blinker(p1) given blinker(p2)' do
+			expect(subject.next_generation([[:dead,:dead,:dead],[:alive,:alive,:alive],
+			 [:dead,:dead,:dead]])).
+			to eq([[:dead,:alive,:dead],
+				[:dead,:alive,:dead],[:dead,:alive,:dead]])
+		end
+
+		it 'returns toad(p2) given toad(p1)' do
+			expect(subject.next_generation([[:dead,:dead,:dead,:dead],
+				[:dead,:alive,:alive,:alive], [:alive,:alive,:alive,:dead],
+				[:dead,:dead,:dead,:dead]])).
+			to eq([[:dead,:dead,:alive,:dead],[:alive,:dead,:dead,:alive],
+				[:alive,:dead,:dead,:alive],[:dead,:alive,:dead,:dead]])
+		end
+
+	    
+	end
+
+	context "#find_neighbours" do
+
+		subject = ConwayGame.new([['foo']])
+		
+		it 'returns b for [[a,b]] and 0,0' do 
+			expect(subject.find_neighbours([['a','b']],[0,0])).to eq ['b']
+		end
+
+		it 'returns a,c for [[a,b,c]] and 0,1' do 
+			expect(subject.find_neighbours([['a','b','c']],[0,1])).
+			to match_array ['a','c']
+		end
+
+		it 'returns b,c,d for [[a,b],[c,d]] and 0,0' do 
+			expect(subject.find_neighbours([['a','b'],['c','d']],[0,0])).
+			to match_array ['b','c','d']
+		end
+
+		it 'returns a,b,d for [[a,b],[c,d]] and 1,0' do 
+			expect(subject.find_neighbours([['a','b'],['c','d']],[1,0])).
+			to match_array ['a','b','d']
+		end
+
+		it 'returns a,b,c for [[a,b],[c,d]] and 1,1' do 
+			expect(subject.find_neighbours([['a','b'],['c','d']],[1,1])).
+			to match_array ['a','b','c']
+		end
+
+		it 'returns a,c,d for [[a,b],[c,d]] and 0,1' do 
+			expect(subject.find_neighbours([['a','b'],['c','d']],[0,1])).
+			to match_array ['a','c','d']
+		end
+
+		it 'returns 1,2,3,4,6,7,8,9 for [[1,2,3],[4,5,6],[7,8,9]] and 1,1' do 
+			expect(subject.find_neighbours([[1,2,3],[4,5,6],[7,8,9]],[1,1])).
+			to match_array [1,2,3,4,6,7,8,9]
+		end
+
+		it 'returns 2,5,6 for [[1,2,3],[4,5,6],[7,8,9]] and 0,2' do 
+			expect(subject.find_neighbours([[1,2,3],[4,5,6],[7,8,9]],[0,2])).
+			to match_array [2,5,6]
+		end
+	end
+
+	context '#set_cell_status' do 
+
+		subject = ConwayGame.new([['foo']])
+
+		it 'returns :alive for :alive and [:alive,:alive,:alive]' do 
+			expect(subject.set_cell_status(:alive,[:alive,:alive,:alive])).
+			to eq :alive
+		end
+
+		it 'returns :dead for :dead and [:dead,:dead,:alive]' do 
+			expect(subject.set_cell_status(:dead,[:dead,:dead,:alive])).
+			to eq :dead
+		end
+
+		it 'returns :dead for :dead and [:dead,:alive,:dead]' do 
+			expect(subject.set_cell_status(:dead,[:dead,:alive,:dead])).
+			to eq :dead
+		end
+
+		it 'returns :alive for :alive [:alive,:alive,:dead]' do 
+			expect(subject.set_cell_status(:alive,[:alive,:alive,:dead])).
+			to eq :alive
+		end
+
+		it 'returns :dead for :dead [:alive,:alive,:dead]' do 
+			expect(subject.set_cell_status(:dead,[:alive,:alive,:dead])).
+			to eq :dead
+		end
+
+		it 'returns :alive for :dead [:alive,:alive,:alive]' do 
+			expect(subject.set_cell_status(:dead,[:alive,:alive,:alive])).
+			to eq :alive
+		end
+
+		it 'returns :dead for :alive [:dead,:dead,:alive]' do 
+			expect(subject.set_cell_status(:alive,[:dead,:dead,:alive])).
+			to eq :dead
+		end
+
+		it 'returns :dead for :alive [:alive,:alive,:alive,:alive]' do 
+			expect(subject.set_cell_status(:alive,[:alive,:alive,:alive,:alive])).
+			to eq :dead
+		end
+
 	end
 end
