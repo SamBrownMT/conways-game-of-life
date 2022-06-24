0bd8c0d60da4d94dbf07fc2b124eac1101e2d262
Sam Brown <sam.brown@madetech.com>
2022-06-24 16:51:38 +0100

    further Sinatra musings

--- conway_app.rb
+++ conway_app.rb
@@ -1,18 +1,23 @@
 require 'sinatra'
 require './conway'
+require 'yaml'
 # get site
 # post new grid
 # get new grid
 # post start loop
+ 
+enable :sessions
 
 get '/' do 
 	erb :home
 end
 
 post '/' do 
-	@subject = ConwayGame.new([[:dead,:dead,:dead],[:alive,:alive,:alive],
-			 [:dead,:dead,:dead]])
-	code = '<%= @subject.next_generation %>'
-	erb code
+	@grid = params["grid"]
+	session[:grid] = @grid
+	redirect '/game'
+end
 
+get '/game' do 
+	erb :game, :locals => {:grid => session[:grid]}
 end
